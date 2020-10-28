99fddf245d9d52de8027eb6540486f2b0427855e
estopon <estopon@gmail.com>
2020-10-28 15:52:08 +0100

    Funcionalidad WishInput

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import WishInput from './Wishinput';
 import WishList from './Wishlist';
 
-const wishes = [
+const initialWishes = [
   { text: 'Travel to the moon', done: false },
   { text: 'Pay the gym', done: true },
   { text: 'Go to the gym', done: false },
 ];
 
-const App = () => (
-  <div className="app">
-    <h1>My wishlist</h1>
-    <WishInput />
-    <WishList wishes={wishes} />
-    <button className="wish-clear" type="button">Archive done</button>
-  </div>
-);
+const App = () => {
+  const [wishes, setWishes] = useState(initialWishes);
+  return (
+    <div className="app">
+      <h1>My wishlist</h1>
+      <WishInput onNewWish={(wish) => setWishes([wish, ...wishes])} />
+      <WishList wishes={wishes} />
+      <button className="wish-clear" type="button">Archive done</button>
+    </div>
+  );
+};
 
 export default App;
