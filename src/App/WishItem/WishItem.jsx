3ffec46433b9b69dd7bc81ca2e2a669c8263d4b6
estopon <estopon@gmail.com>
2020-11-03 14:00:23 +0100

    Colores según antigüedad de elementos

--- src/App/WishItem/WishItem.jsx
+++ src/App/WishItem/WishItem.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const WishItem = ({
   done, text, id, onDoneChange,
-}) => (
-  <li key={text} className={classNames('wish-list__item', { 'wish-list__item--done': done })}>
-    <input id={id} type="checkbox" checked={done} onChange={(e) => onDoneChange(e.target.checked)} />
-    <label htmlFor={id}>
-      {text}
-    </label>
-  </li>
-);
+}) => {
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    let ageInterval;
+    if (done) {
+      setAge(0);
+    } else {
+      ageInterval = setInterval(() => {
+        if (done) {
+          clearInterval(ageInterval);
+        } else {
+          setAge((a) => a + 1);
+        }
+      }, 1000);
+    }
+    return () => clearInterval(ageInterval);
+  }, [done]);
+
+  return (
+    <li key={text} className={classNames('wish-list__item', { 'wish-list__item--done': done, 'wish-list__item--warning': age >= 5 && age < 10, 'wish-list__item--danger': age >= 10 })}>
+      <input id={id} type="checkbox" checked={done} onChange={(e) => onDoneChange(e.target.checked)} />
+      <label htmlFor={id}>
+        {text}
+      </label>
+    </li>
+  );
+};
 
 WishItem.propTypes = {
   done: PropTypes.bool,
